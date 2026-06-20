594757af6cc959757c0b8d6d25b4ad90271dc4d1
selvanatadroshanna-sys <selvanatadroshanna@gmail.com>
2026-06-20 15:42:53 +0300

    Update home page 3

--- HCP.py
+++ HCP.py
@@ -106,23 +106,6 @@ st.markdown("""
     color: #64748b;
 }
 
-div.stButton > button {
-    width: 100%;
-    height: 130px;
-    border-radius: 24px;
-    font-size: 24px;
-    font-weight: 800;
-    border: 1px solid #cbd5e1;
-    background: white;
-    color: #0f172a;
-    box-shadow: 0 8px 24px rgba(15, 23, 42, 0.08);
-}
-
-div.stButton > button:hover {
-    background: #2563eb;
-    color: white;
-    border-color: #2563eb;
-}
 
 @media (prefers-color-scheme: dark) {
     .stApp {
@@ -143,20 +126,24 @@ div.stButton > button:hover {
     .card-text, .kpi-title {
         color: #cbd5e1;
     }
-
+}
 div.stButton > button {
     width: 100%;
-    height: 220px;
+    height: 170px;
     border-radius: 30px;
-    font-size: 34px;
+    font-size: 30px;
     font-weight: 800;
-    border: none;
-    background: linear-gradient(135deg,#2563eb,#1d4ed8);
+    border: 1px solid rgba(255,255,255,0.45);
+    background: rgba(255,255,255,0.18);
     color: white;
-    box-shadow: 0 12px 30px rgba(37,99,235,.25);
+    backdrop-filter: blur(14px);
+    box-shadow: 0 12px 35px rgba(0,0,0,0.25);
 }
 
 div.stButton > button:hover {
+    background: rgba(255,255,255,0.30);
+    color: white;
+    border-color: rgba(255,255,255,0.75);
     transform: translateY(-5px);
     transition: all .3s ease;
 }
@@ -267,7 +254,17 @@ if page == "Home":
         </div>
     </div>
     """, unsafe_allow_html=True)
-
+    st.markdown("""
+    <style>
+    div[data-testid="stHorizontalBlock"]{
+        margin-top:-250px;
+        padding-left:60px;
+        padding-right:60px;
+        position:relative;
+        z-index:999;
+    }
+    </style>
+    """, unsafe_allow_html=True)
     st.markdown("### Go To")
 
     nav1, nav2 = st.columns(2)
